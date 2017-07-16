0253bd30a35195139ccb5df662cb68633826ddbb
Edward Flathers <flathers+gh@gmail.com>
2017-07-16 16:06:33 -0700

    geohashes

--- eda/tmine.R
+++ eda/tmine.R
@@ -1,19 +1,11 @@
 
-tokens <- scan("/home/flathers/Desktop/workspace/search-log-analysis/data/tokens.txt", what="string", sep="\n")
+tokens <- scan("/home/flathers/Desktop/workspace/search-log-analysis/data/tokens2.txt", what="string", sep="\n")
 t = as.data.frame(tolower(tokens))
 summary(t)
 head(table(t), 100)
 
-tolower(t)
-
-
-sort(table(t), decreasing=TRUE)
-
-
 library(tm)
-
 stopWords <- stopwords("en")
-
 
 t2 = tolower(tokens)
 t2 = t2[! t2 %in% stopWords]
@@ -22,8 +14,9 @@ head(sort(table(t2), decreasing=TRUE))
 
 library(wordcloud)
 df = as.data.frame(sort(table(t2), decreasing=TRUE))
-set.seed(1234)
+set.seed(12345)
 wordcloud(words = df$t2, freq = df$Freq, min.freq = 1,
-          max.words=500, random.order=FALSE, rot.per=0.35, 
+          max.words=200, random.order=FALSE, rot.per=0.35, 
           colors=brewer.pal(8, "Dark2"))
 
+head(df, 20)
